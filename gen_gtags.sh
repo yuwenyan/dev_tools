d4084769deb36b0685c32f1454cc24782359d2c4
wyu <wyu@fortinet.com>
2020-03-04 11:23:03 -0800

    update

--- gen_gtags.sh
+++ gen_gtags.sh
@@ -6,7 +6,8 @@ echo Gtags Creating V0.01
 echo [1/2] Searching files ...
 
 # find . -name *.[ch] > gtags.files
-find . -name '*.c' -o -name '*.cpp' -o -name '*.h' > gtags.files
+# ignore "./frc_core"
+find . d \( -path ./frc_core -o -path ./faz_core/tools \) -prune -o -name '*.c' -o -name '*.cpp' -o -name '*.h' > gtags.files
 
 echo Done.
 
